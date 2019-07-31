c942e2267045ff34f44bebd6276f0c39f153b685
Nemesiss.Lin <353388639@qq.com>
2019-07-31 11:14:16 +0800

    Auto create static file folder

--- Services/FileUpload/StaticFilePathResovler.cs
+++ Services/FileUpload/StaticFilePathResovler.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using Microsoft.Extensions.Logging;
 
 namespace OtokatariBackend.Utils
 {
@@ -9,7 +10,18 @@ namespace OtokatariBackend.Utils
         public string sharing { get; set; }
 
 
-        public string GetAvatar() => Path.Combine(root,avatar);
-        public string GetSharing() => Path.Combine(root,sharing);
+        public string GetAvatar() => Path.Combine(root, avatar);
+        public string GetSharing() => Path.Combine(root, sharing);
+        public StaticFilePathResovler()
+        {
+            var dirs = new[] { root, GetAvatar(), GetSharing() };
+            foreach (var dir in dirs)
+            {
+                var DirInfo = new DirectoryInfo(dir);
+                if(!DirInfo.Exists)
+                    DirInfo.Create();
+                System.Console.WriteLine($"Static file folder {dir} loaded");
+            }
+        }
     }
 }
