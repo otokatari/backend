14d439f771522fddbd5f2fd25197e39fbc6150af
Nemesiss.Lin <353388639@qq.com>
2019-08-05 20:01:11 +0800

    Fix DateUtils code mistake

--- Utils/DateUtil.cs
+++ Utils/DateUtil.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace OtokatariBackend.Utils
 {
     public static class DateUtil
     {
-        public static long DateToUnix(this DateTime dt) => (long)(DateTime.Now.ToUniversalTime() - new DateTime(1970, 1, 1)).TotalSeconds;
+        public static long DateToUnix(this DateTime dt) => (long)(dt.ToUniversalTime() - new DateTime(1970, 1, 1)).TotalSeconds;
 
         public static DateTime UnixToDate(long unix) => new DateTime(1970, 1, 1).AddSeconds(unix);
 
