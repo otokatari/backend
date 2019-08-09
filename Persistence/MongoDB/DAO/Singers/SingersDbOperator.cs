8bf5faa96c676585d4972b4865326fcbf1b06b08
Nemesiss.Lin <353388639@qq.com>
2019-08-09 08:33:16 +0800

    Fix singer db operator field definition builder

--- Persistence/MongoDB/DAO/Singers/SingersDbOperator.cs
+++ Persistence/MongoDB/DAO/Singers/SingersDbOperator.cs
@@ -96,7 +96,7 @@ namespace OtokatariBackend.Persistence.MongoDB.DAO.DbSingers
         public Singers GetSingerInfoByObjectId(ObjectId SingerObjid) 
                         => GetSingerInfo(f => f.Eq(x => x._id, SingerObjid));
         public Singers GetSingerInfoByPlatformId(string Singerid) 
-                        => GetSingerInfo(f => f.AnyEq(x => new[] { x.NeteaseId, x.KugouId, x.QQMusicId }, Singerid));
+                        => GetSingerInfo(f => f.Or((new[] {"NeteaseId", "KugouId", "QQMusicId" }).Select(y => f.Eq(y,Singerid))));
         public Singers GetSingerInfoByName(string SingerName) 
                         => GetSingerInfo((f) => f.Eq(x => x.SingerName, SingerName));
 
